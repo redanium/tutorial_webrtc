924662c7e4bc443203155fc62e24365c89597c61
David <david.bui@cosmosoftware.io>
2017-09-22 14:55:23 +0800

    ENH: Qt5 example with Signal slot

--- Qt5App/main.cpp
+++ Qt5App/main.cpp
@@ -1,12 +1,19 @@
 #include <QApplication>
 #include <QPushButton>
-#include <QLabel>
+
 
 int main(int argc, char** argv){
+	//Main application
 	QApplication app(argc, argv);
-    QPushButton *button = new QPushButton ("Hello World!");
- 
-	//QObject::connect(button,SIGNAL(clicked()), button,SLOT(quit()));
-
+	//A button with Quit as text
+    QPushButton button("Quit");
+ 	
+ 	//Main feature of Qt : Signal / Slot
+ 	// Basically a Signal is an event
+ 	// and the slot is a method called for that event
+ 	//This is connecting the button to the method quit() of the app
+	QObject::connect(&button,SIGNAL(clicked()), &app,SLOT(quit()));
+	//Show the button
+	button.show();
     return app.exec();
 }
