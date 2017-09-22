e05f0ce2a8624fa071e405ca677e46f12aecde28
David Bui <david.bui@cosmosoftware.io>
2017-09-22 15:15:39 +0800

    Update main.cpp
    Indent with spaces please.

--- Qt5App/main.cpp
+++ Qt5App/main.cpp
@@ -3,17 +3,17 @@
 
 
 int main(int argc, char** argv){
-	//Main application
-	QApplication app(argc, argv);
-	//A button with Quit as text
-    QPushButton button("Quit");
+  //Main application
+  QApplication app(argc, argv);
+  //A button with Quit as text
+  QPushButton button("Quit");
  	
- 	//Main feature of Qt : Signal / Slot
- 	// Basically a Signal is an event
- 	// and the slot is a method called for that event
- 	//This is connecting the button to the method quit() of the app
-	QObject::connect(&button,SIGNAL(clicked()), &app,SLOT(quit()));
-	//Show the button
-	button.show();
-    return app.exec();
+  //Main feature of Qt : Signal / Slot
+  // Basically a Signal is an event
+  // and the slot is a method called for that event
+  //This is connecting the button to the method quit() of the app
+  QObject::connect(&button,SIGNAL(clicked()), &app,SLOT(quit()));
+  //Show the button
+  button.show();
+  return app.exec();
 }
